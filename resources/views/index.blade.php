f247bb1d14701efce35c479895902508ff6f3c7f
Klebson Mateus <klebsonbm@gmail.com>
2017-01-04 18:00:51 -0200

    Bora Porra

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Versão Beta</title>
+    <title>Bora Porra!</title>
 
     <link rel="stylesheet" href="{{ elixir('css/vendor.css') }}">
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
